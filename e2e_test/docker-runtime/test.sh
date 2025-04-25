89956a8c8c7d3d650065b511d8a997f196a8eb53
Eric Vallee <eric_vallee@webificservices.com>
2025-04-24 23:57:24 -0400

    fix: INFRA-952 Updated golang version in test runtime wrapper

--- e2e_test/docker-runtime/test.sh
+++ e2e_test/docker-runtime/test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -e
 
-docker run -v $(pwd):/opt/code -v $(pwd)/e2e_test/docker-runtime/entrypoint.sh:/opt/entrypoint.sh -w /opt --rm --entrypoint="/opt/entrypoint.sh" golang:1.20-bullseye
+docker run -v $(pwd):/opt/code -v $(pwd)/e2e_test/docker-runtime/entrypoint.sh:/opt/entrypoint.sh -w /opt --rm --entrypoint="/opt/entrypoint.sh" golang:1.23-bullseye
